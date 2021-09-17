190ba18cce8048329ac374c8e31e0d761bc67f16
charindo <charindou@gmail.com>
2021-09-17 17:31:50 +0900

    Embedをいじってみた

--- src/narumi.js
+++ src/narumi.js
@@ -1,4 +1,5 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
+
 const dotenv = require('dotenv');
 dotenv.config();
 if (process.env.TOKEN == 'undefined') {
@@ -6,15 +7,39 @@ if (process.env.TOKEN == 'undefined') {
   process.exit(1);
 }
 
+// ================================================================ //
+const prefix = "nr!";
+// ================================================================ //
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`${client.user.tag} でログインしています。`)
 })
 
-client.on('messageCreate', async msg => {
-  if (msg.content === '!ping') {
-    msg.channel.send('Pong!')
+client.on('messageCreate', async message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!')
+  }else if(message.content === '!test'){
+    const exampleEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Some title')
+      .setURL('https://discord.js.org/')
+      .setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+      .setDescription('Some description here')
+      .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+      .addFields(
+        { name: 'Regular field title', value: 'Some value here' },
+        { name: '\u200B', value: '\u200B' },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+        { name: 'Inline field title', value: 'Some value here', inline: true },
+      )
+      .addField('Inline field title', 'Some value here', true)
+      .setImage('https://i.imgur.com/AfFp7pu.png')
+      .setTimestamp()
+      .setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+    message.channel.send({ embeds: [exampleEmbed] });
   }
 }) 
 
